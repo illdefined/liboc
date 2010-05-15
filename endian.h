27e1a122dbfe630f0c5c06cc896f8fd5883211a1
Mikael Voss <mikael@illdefined.org>
2010-05-15 11:43:56 +0000

    Use macros instead of inline functions

--- endian.h
+++ endian.h
@@ -8,89 +8,49 @@
 #define LITTLE_ENDIAN 1234
 #define BYTE_ORDER BIG_ENDIAN
 
-inline uint16_t swap16(uint16_t x) {
-	return
-		(x & UINT16_C(0xff00)) >> 8 |
-		(x & UINT16_C(0x00ff)) << 8;
-}
+#define swap16(x) \
+	(((x) & UINT16_C(0xff00)) >> 8 | \
+	 ((x) & UINT16_C(0x00ff)) << 8)
 
-inline uint32_t swap32(uint32_t x) {
-	if (__builtin_constant_p(x))
-		return
-			(x & UINT32_C(0xff000000)) >> 24 |
-			(x & UINT32_C(0x00ff0000)) >>  8 |
-			(x & UINT32_C(0x0000ff00)) <<  8 |
-			(x & UINT32_C(0x000000ff)) << 24;
-	else
-		return __builtin_bswap32(x);
-}
+#define swap32(x) \
+	(__builtin_constant_p(x) ? \
+		((x) & UINT32_C(0xff000000)) >> 24 | \
+		((x) & UINT32_C(0x00ff0000)) >>  8 | \
+		((x) & UINT32_C(0x0000ff00)) <<  8 | \
+		((x) & UINT32_C(0x000000ff)) << 24 : \
+		__builtin_bswap32(x))
 
-inline uint32_t swap64(uint64_t x) {
-	if (__builtin_constant_p(x))
-		return
-			(x & UINT64_C(0xff00000000000000)) >> 56 |
-			(x & UINT64_C(0x00ff000000000000)) >> 40 |
-			(x & UINT64_C(0x0000ff0000000000)) >> 24 |
-			(x & UINT64_C(0x000000ff00000000)) >>  8 |
-			(x & UINT64_C(0x00000000ff000000)) <<  8 |
-			(x & UINT64_C(0x0000000000ff0000)) << 24 |
-			(x & UINT64_C(0x000000000000ff00)) << 40 |
-			(x & UINT64_C(0x00000000000000ff)) << 56;
-	else
-		return __builtin_bswap64(x);
-}
+#define swap64(x) \
+	(__builtin_constant_p(x) ? \
+			((x) & UINT64_C(0(x)ff00000000000000)) >> 56 | \
+			((x) & UINT64_C(0(x)00ff000000000000)) >> 40 | \
+			((x) & UINT64_C(0(x)0000ff0000000000)) >> 24 | \
+			((x) & UINT64_C(0(x)000000ff00000000)) >>  8 | \
+			((x) & UINT64_C(0(x)00000000ff000000)) <<  8 | \
+			((x) & UINT64_C(0(x)0000000000ff0000)) << 24 | \
+			((x) & UINT64_C(0(x)000000000000ff00)) << 40 | \
+			((x) & UINT64_C(0(x)00000000000000ff)) << 56 : \
+			__builtin_bswap64((x))
 
 #if BYTE_ORDER == BIG_ENDIAN
 
-inline uint16_t tobe16(uint16_t x) {
-	return x;
-}
+#define tobe16(x) x
+#define tobe32(x) x
+#define tobe64(x) x
 
-inline uint32_t tobe32(uint32_t x) {
-	return x;
-}
-
-inline uint64_t tobe64(uint64_t x) {
-	return x;
-}
-
-inline uint16_t tole16(uint16_t x) {
-	return swap16(x);
-}
-
-inline uint32_t tole32(uint32_t x) {
-	return swap32(x);
-}
-
-inline uint64_t tole64(uint64_t x) {
-	return swap64(x);
-}
+#define tole16(x) swap16(x)
+#define tole32(x) swap32(x)
+#define tole64(x) swap64(x)
 
 #elif BYTE_ORDER == LITTLE_ENDIAN
 
-inline uint16_t tobe16(uint16_t x) {
-	return swap16(x);
-}
+#define tobe16(x) swap16(x)
+#define tobe32(x) swap32(x)
+#define tobe64(x) swap64(x)
 
-inline uint32_t tobe32(uint32_t x) {
-	return swap32(x);
-}
-
-inline uint64_t tobe64(uint64_t x) {
-	return swap64(x);
-}
-
-inline uint16_t tole16(uint16_t x) {
-	return x;
-}
-
-inline uint32_t tole32(uint32_t x) {
-	return x;
-}
-
-inline uint64_t tole64(uint64_t x) {
-	return x;
-}
+#define tole16(x) x
+#define tole32(x) x
+#define tole64(x) x
 
 #else
 #error "Unknown byte‐order!"
