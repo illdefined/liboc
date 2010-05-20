d81854e1cb1be902c3b9539e38998e885ca08c36
Mikael Voss <mikael@illdefined.org>
2010-05-20 15:07:55 +0000

    Doxygen annotations

--- endian.h
+++ endian.h
@@ -2,6 +2,12 @@
 #ifndef OC_ENDIAN_H
 #define OC_ENDIAN_H
 
+/**
+ * \file
+ *
+ * \brief Byte‐order conversion macros.
+ */
+
 #include <stdint.h>
 
 #undef BIG_ENDIAN
@@ -12,10 +18,24 @@
 #define LITTLE_ENDIAN 1234
 #define BYTE_ORDER BIG_ENDIAN
 
+/**
+ * \brief Swap octets of a 16bit integer.
+ *
+ * \param x Integer.
+ *
+ * \return Swapped integer.
+ */
 #define swap16(x) \
 	(((x) & UINT16_C(0xff00)) >> 8 | \
 	 ((x) & UINT16_C(0x00ff)) << 8)
 
+/**
+ * \brief Swap octets of a 32bit integer.
+ *
+ * \param x Integer.
+ *
+ * \return Swapped integer.
+ */
 #define swap32(x) \
 	(__builtin_constant_p(x) ? \
 		((x) & UINT32_C(0xff000000)) >> 24 | \
@@ -24,6 +44,13 @@
 		((x) & UINT32_C(0x000000ff)) << 24 : \
 		__builtin_bswap32(x))
 
+/**
+ * \brief Swap octets of a 64bit integer.
+ *
+ * \param x Integer.
+ *
+ * \return Swapped integer.
+ */
 #define swap64(x) \
 	(__builtin_constant_p(x) ? \
 			((x) & UINT64_C(0xff00000000000000)) >> 56 | \
@@ -35,6 +62,54 @@
 			((x) & UINT64_C(0x000000000000ff00)) << 40 | \
 			((x) & UINT64_C(0x00000000000000ff)) << 56 : \
 			__builtin_bswap64(x))
+
+/**
+ * \def tobe16(x)
+ *
+ * \brief Convert native 16bit integer to big‐endian byte‐order.
+ *
+ * \return Big‐endian integer.
+ */
+
+/**
+ * \def tobe32(x)
+ *
+ * \brief Convert native 32bit integer to big‐endian byte‐order.
+ *
+ * \return Big‐endian integer.
+ */
+
+/**
+ * \def tobe64(x)
+ *
+ * \brief Convert native 64bit integer to big‐endian byte‐order.
+ *
+ * \return Big‐endian integer.
+ */
+
+/**
+ * \def tole16(x)
+ *
+ * \brief Convert native 16bit integer to little‐endian byte‐order.
+ *
+ * \return Little‐endian integer.
+ */
+
+/**
+ * \def tole32(x)
+ *
+ * \brief Convert native 32bit integer to little‐endian byte‐order.
+ *
+ * \return Little‐endian integer.
+ */
+
+/**
+ * \def tole64(x)
+ *
+ * \brief Convert native 64bit integer to little‐endian byte‐order.
+ *
+ * \return Little‐endian integer.
+ */
 
 #if BYTE_ORDER == BIG_ENDIAN
 
