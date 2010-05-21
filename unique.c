ae6ff48c6176a6c4f3b0cbacc1bf95959ae41eb1
Mikael Voss <mikael@illdefined.org>
2010-05-21 11:17:23 +0000

    Unique identifier test routine

--- unique.c
+++ unique.c
@@ -69,3 +69,78 @@ bool unique(uint64_t id[restrict 4]) {
 
 	return true;
 }
+
+#ifdef TEST
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * \brief Number of test identifiers to generate.
+ */
+#define UNIQUE_COUNT 16384
+
+/**
+ * \brief Calculate hamming distance between two identifiers.
+ *
+ * \param x First identifier.
+ * \param y Second identifier.
+ *
+ * \return Hamming distance between \a x and \a y.
+ */
+static inline unsigned int hamming(uint64_t x[restrict 4], uint64_t y[restrict 4]) {
+	unsigned int distance = 0;
+
+	for (size_t iter = 0; iter < 4; ++iter) {
+#if defined(__clang__) || defined(__GNUC__)
+	distance += (unsigned int) __builtin_popcountll(x[iter] ^ y[iter]);
+#else
+	for (size_t jter = 0; jter < sizeof (uint64_t) * 8; ++jter)
+		if ((x[iter] ^ y[iter]) & UINT64_C(1) << jter)
+			++distance;
+#endif
+	}
+
+	return distance;
+}
+
+/**
+ * \brief Unique identifier test routine.
+ *
+ * \return \c EXIT_SUCCESS if successful or \c EXIT_FAILURE on failure.
+ */
+int main(void) {
+	uint64_t *ids = calloc(UNIQUE_COUNT, 4 * sizeof (uint64_t));
+	if (!ids) {
+		perror("calloc");
+		return EXIT_FAILURE;
+	}
+
+	/* Generate test identifiers */
+	for (size_t iter = 0; iter < UNIQUE_COUNT; ++iter) {
+		if (!unique(&ids[iter * 4])) {
+			perror("unique");
+			return EXIT_FAILURE;
+		}
+	}
+
+	unsigned long acc = 0;
+
+	/* Calculate hamming distance */
+	for (size_t iter = 0; iter < UNIQUE_COUNT; ++iter)
+		for (size_t jter = iter + 1; jter < UNIQUE_COUNT; ++jter)
+			acc += hamming(&ids[iter * 4], &ids[jter * 4]);
+
+	double avg = (double) acc / (double) (UNIQUE_COUNT * UNIQUE_COUNT - UNIQUE_COUNT);
+	printf("unique: Average hamming distance %f\n", avg);
+
+	/* FIXME: This value was choosen arbitrarily */
+	if (avg < 8.0) {
+		fputs("Average hamming distance unreasonably low\n", stderr);
+		return EXIT_FAILURE;
+	}
+
+	free(ids);
+
+	return EXIT_SUCCESS;
+}
+#endif
