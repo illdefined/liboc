ec5e0267850578c51d24e8a2ed914813ad7f93fd
Mikael Voss <mikael@illdefined.org>
2010-05-31 16:12:56 +0000

    File system paths

---
+++ path.h
@@ -0,0 +1,36 @@
+#pragma once
+#ifndef OC_PATH_H
+#define OC_PATH_H
+
+/**
+ * \file
+ *
+ * \brief File system paths.
+ */
+
+/**
+ * \brief Base path for cached data.
+ */
+#define CACHE_BASE "/var/cache/opencorpus/"
+
+/**
+ * \brief Base path for lock files.
+ */
+#define LOCK_BASE "/var/cache/opencorpus/"
+
+/**
+ * \brief Base path for log files.
+ */
+#define LOG_BASE "/var/log/opencorpus/"
+
+/**
+ * \brief Base path for architecture‐independent data
+ */
+#define SHARE_BASE "/usr/share/opencorpus/"
+
+/**
+ * \brief Base path for temporary files.
+ */
+#define TEMP_BASE "/var/tmp/opencorpus/"
+
+#endif
